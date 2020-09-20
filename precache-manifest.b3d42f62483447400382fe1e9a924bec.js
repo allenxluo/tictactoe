73ee1e8953a0190ce0c079739a397eb7438493aa
Allen Luo <allenxluo@berkeley.edu>
2020-09-19 21:49:55 -0700

    Updates

---
+++ precache-manifest.b3d42f62483447400382fe1e9a924bec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e11de584e35414630769c95e5c53132",
+    "url": "/allenxluo/tictactoe/index.html"
+  },
+  {
+    "revision": "20b27212960aeb7b4d8b",
+    "url": "/allenxluo/tictactoe/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/allenxluo/tictactoe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/allenxluo/tictactoe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20b27212960aeb7b4d8b",
+    "url": "/allenxluo/tictactoe/static/js/main.bf1796b3.chunk.js"
+  },
+  {
+    "revision": "f94b6333230ff115b469",
+    "url": "/allenxluo/tictactoe/static/js/runtime-main.5671db6e.js"
+  }
+]);
